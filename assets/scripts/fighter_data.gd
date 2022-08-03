ed828eb12987c70ebb697e30b1fe9e3624c27ac4
erik-lance <eriklance@gmail.com>
2022-08-04 01:40:06 +0800

    Create fighter_data.gd

---
+++ assets/scripts/fighter_data.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+signal hit_hp(hp)
+signal use_mp(mp)
+
+var max_hp
+var cur_hp
+
+var max_mp
+var cur_mp
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func ready_fighter(hp=100, mp=100):
+	max_hp = hp
+	cur_hp = hp
+	
+	max_mp = mp
+	cur_mp = mp
+
+func regen_mp(mp):
+	cur_mp +=  mp
+	if cur_mp > max_mp: cur_mp = max_mp
+
+func take_damage(dmg):
+	cur_hp -= dmg
+	if cur_hp < 0: cur_hp = 0
+	emit_signal('hit_hp', cur_hp)
+
+func use_mp(mp):
+	cur_mp -= mp
+	emit_signal('use_mp', cur_mp)
+	
