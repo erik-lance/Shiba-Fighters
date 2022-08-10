c93388f0487627ffc9da5e0d1f830f6ee052af32
erik-lance <eriklance@gmail.com>
2022-08-10 22:04:18 +0800

    Moved pivot, grid is now always visible but with alpha 0 due to grid reacting based on visibility as well so this has been fixed

--- assets/scripts/cards/card_ui.gd
+++ assets/scripts/cards/card_ui.gd
@@ -1,21 +1,21 @@
 extends Node2D
 
-onready var card_title = $TextureRect/Title
-onready var hp = $TextureRect/Numbers/HPLabel
-onready var mp = $TextureRect/Numbers/MPLabel
+onready var card_title = $Title
+onready var hp = $Numbers/HPLabel
+onready var mp = $Numbers/MPLabel
 
-onready var grid = $TextureRect/Grid
+onready var grid = $Grid
 
 var grid_space = {
-	0: $TextureRect/Grid/TOP_LEFT,
-	1: $TextureRect/Grid/TOP_CENTER,
-	2: $TextureRect/Grid/TOP_RIGHT,
-	3: $TextureRect/Grid/MID_LEFT,
-	4: $TextureRect/Grid/MID_CENTER,
-	5: $TextureRect/Grid/MID_RIGHT,
-	6: $TextureRect/Grid/BOT_LEFT,
-	7: $TextureRect/Grid/BOT_CENTER,
-	8: $TextureRect/Grid/BOT_RIGHT
+	0: $Grid/TOP_LEFT,
+	1: $Grid/TOP_CENTER,
+	2: $Grid/TOP_RIGHT,
+	3: $Grid/MID_LEFT,
+	4: $Grid/MID_CENTER,
+	5: $Grid/MID_RIGHT,
+	6: $Grid/BOT_LEFT,
+	7: $Grid/BOT_CENTER,
+	8: $Grid/BOT_RIGHT
 }
 
 func set_card_data(card_data):
@@ -26,7 +26,6 @@ func set_card_data(card_data):
 	
 	# Check for move tiles
 	for tile in card_data.get_child(0).get_tiles():
-		grid_space[tile].visible = true
 		grid_space[tile].color = Color('#6eff00')
 	
 	pass
