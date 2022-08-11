512dd7fa56bce2bd52a38f9efa776e4261036fb3
erik-lance <eriklance@gmail.com>
2022-08-11 21:03:22 +0800

    Connected signals to fighters through arena manager as well as a system for turntaking

--- assets/scripts/fight_manager.gd
+++ assets/scripts/fight_manager.gd
@@ -3,25 +3,57 @@ extends Node2D
 onready var arena_manager = $ArenaManager
 onready var deck_manager = $DeckManager
 onready var screen = $StrategyScreen
+onready var anim = $AnimationPlayer
 
 var round_num = 0
 
 var max_turn = 3
-var cur_turn = -1
+var cur_turn = 0
+
+var player_cards = null
+var enemy_cards = null
+
+var fighter = {
+	player = null,
+	enemy = null
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	arena_manager.connect("arena_finished",self,'_on_arena_loaded')
+	arena_manager.connect("player_finished",self,'player_done')
+	arena_manager.connect("enemy_finished",self,'enemy_done')
+	
+	arena_manager.prepare_tester()
 	screen.set_deck(deck_manager.get_shiba_cards())
+	anim.play("strat_screen_open")
+	screen.connect("begin",self,'_on_battle_begin')
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _on_arena_loaded(player,enemy):
+	fighter.player = player
+	fighter.enemy = enemy
+	print('Arena has successfully loaded!')
 
+func _on_battle_begin(cards):
+	player_cards = cards
+	anim.play("strat_screen_close")
+	battle_director()
 
-func _on_battle_begin():
-	screen.visible = false
+func battle_director():
+	print(fighter.player)
+	fighter.player.get_child(0).play_anim(player_cards[cur_turn].get_anim_name())
+	pass
 
+func player_done(anim_name):
+	pass
 
-func _on_StrategyScreen_begin():
-	screen.visible = false
+func enemy_done(anim_name):
+	pass
+
+func animation_done():
+	pass
