ce61ec9a21b9aef70b9474bf5d94647986cdd43b
erik-lance <eriklance@gmail.com>
2022-08-11 19:10:52 +0800

    Converted grid from dictionary to array. Also checks for damage tiles now

--- assets/scripts/cards/card_ui.gd
+++ assets/scripts/cards/card_ui.gd
@@ -6,35 +6,42 @@ onready var mp = $Numbers/MPLabel
 
 onready var grid = $Grid
 
-signal card_selected()
+var card_ref = null
 
-func _ready():
-	disable_button()
+signal card_selected(ref)
 
-var grid_space = {
-	0: $Grid/TOP_LEFT,
-	1: $Grid/TOP_CENTER,
-	2: $Grid/TOP_RIGHT,
-	3: $Grid/MID_LEFT,
-	4: $Grid/MID_CENTER,
-	5: $Grid/MID_RIGHT,
-	6: $Grid/BOT_LEFT,
-	7: $Grid/BOT_CENTER,
-	8: $Grid/BOT_RIGHT
-}
+onready var grid_space = [
+	$Grid/TOP_LEFT,
+	$Grid/TOP_CENTER,
+	$Grid/TOP_RIGHT,
+	$Grid/MID_LEFT,
+	$Grid/MID_CENTER,
+	$Grid/MID_RIGHT,
+	$Grid/BOT_LEFT,
+	$Grid/BOT_CENTER,
+	$Grid/BOT_RIGHT
+]
 
 func set_card_data(card_data):
+	card_ref = card_data
+	
 	hp.text = str(card_data.get_dmg())
 	mp.text = str(card_data.get_mana())
 	
 	card_title.text = card_data.get_card_name()
+	self.name = card_title.text
 	
 	# Check for move tiles
-	for tile in card_data.get_child(0).get_tiles():
-		grid_space[tile].color = Color('#6eff00')
+	if card_data.get_child(0).get_tiles() != null:
+		for tile in card_data.get_child(0).get_tiles():
+			grid_space[tile].color = Color('#6eff00')
+
+	if card_data.get_child(1).get_tiles() != null:
+		for tile in card_data.get_child(1).get_tiles():
+			grid_space[tile].color = Color.red
 
 func disable_button():
 	$Button.disabled
 
 func _on_Button_button_up():
-	emit_signal("card_selected")
+	emit_signal("card_selected", card_ref)
