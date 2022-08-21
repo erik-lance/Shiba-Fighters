0b9feed122a6dc5d592fc63bfd7d5ddf7ddc174c
erik-lance <eriklance@gmail.com>
2022-08-21 22:52:51 +0800

    Created a scene just for FX handling for each fighter. This uses a 3x3 grid system with an extra sprite in the center

---
+++ assets/scripts/fx_handler.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+onready var fx_tiles = [
+	$FXTopLeft,
+	$FXTop,
+	$FXTopRight,
+	$FXLeft,
+	$FXMid,
+	$FXRight,
+	$FXBotLeft,
+	$FXBot,
+	$FXBotRight,
+	$FXMain
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func place_on_tile(n=9,obj=null):
+	if obj:
+		print(obj)
+		var instanced_obj = load(obj).instance()
+		fx_tiles[n].add_child(instanced_obj)
+	else:
+		print('Tile is null!')
+
+func clear_tile(n=-1):
+	if n==-1:
+		for tile in fx_tiles:
+			tile.get_child(0).queue_free()
+	else:
+		fx_tiles[n].get_child(0).queue_free()
+
+func sp_sudoku_tile(n):
+	if n < 0 or n > 8:
+		print('Null sudoku tile! given:'+str(n))
+	else:
+		var r = randi() % 9 + 1
+		var sudoku_num = load("res://scenes/arena/fx/sudoku_numbers/1.tscn").instance()
+		fx_tiles[n].add_child(sudoku_num)
+		sudoku_num.get_child(0).text = str(r)
+	
