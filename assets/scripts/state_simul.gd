5472c11dea84ab396e18200f338511b129faa191
erik-lance <eriklance@gmail.com>
2022-08-28 02:19:24 +0800

    Now has signal to root node when finishing and a set of rules commented out

--- assets/scripts/state_simul.gd
+++ assets/scripts/state_simul.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+signal end_state()
+
+var root = null
+var heuristic_rule = null
+var player_AI = null
+
+var depth = -1
+
+var h_value = 0
+var state_link = "res://scenes/state.tscn"
+
+var possible_state = false
+
+var self_turn = false
+var move_num = -1
+
 var cur_state = {
 	player_pos_x = 0,
 	player_pos_y = 0,
@@ -15,21 +31,65 @@ var cur_state = {
 func _ready():
 	pass # Replace with function body.
 
-func prep_dets(dets):
-	cur_state.player_pos_x = dets.player_pos_x
-	cur_state.player_pos_y = dets.player_pos_y
-	cur_state.self_pos_x = dets.self_pos_x
-	cur_state.self_pos_y = dets.self_pos_y
-	cur_state.player_hp = dets.player_hp
-	cur_state.player_mp = dets.player_mp
-	cur_state.self_hp = dets.self_hp
-	cur_state.self_mp = dets.self_mp
+func prep_dets(dets, r, mn=-1, d=-1, possible=false):
+	if possible:
+		root = r
+		cur_state.player_pos_x = dets.player_pos_x
+		cur_state.player_pos_y = dets.player_pos_y
+		cur_state.self_pos_x = dets.self_pos_x
+		cur_state.self_pos_y = dets.self_pos_y
+		cur_state.player_hp = dets.player_hp
+		cur_state.player_mp = dets.player_mp
+		cur_state.self_hp = dets.self_hp
+		cur_state.self_mp = dets.self_mp
+		
+		move_num = mn
+		depth = d
+		
+		# Once reaches depth 3 (Final depth)
+		if d == 3:
+			self.connect('end_state',root,'_on_finish_min_max_state')
+		
+		calculate_state()
+	else:
+		root = r
+		move_num = mn
+		depth = d
+		self.connect('end_state',root,'_on_finish_min_max_state')
+		print('Finished impossible state')
+		if d==3:
+			emit_signal("end_state")
 
-#
-#
-# CALCULATION TOOLS
-#
-#
+func set_possible_state(t=false):
+	possible_state = t
+
+func set_h_value(h):
+	h_value = h
+
+func get_h_value():
+	return h_value
+
+
+# --------------------------------------------------- #
+# ---------------- CALCULATION TOOLS ---------------- #
+# --------------------------------------------------- #
+func calculate_state():
+	# TODO: Add heuristic_rule properly soon	
+	
+	
+	# Heuristic Rules
+	# Calculate Position based on dmg_proximity
+	# Optionals: ------------------------------
+	# Calculate Regen (IF CONFIRMED!)
+	# Calculate Defense (IF CONFIRMED!)
+	# Calculate Attacks (IF CONFIRMED!)
+	# -----------------------------------------
+	# MinMax Final HP/MP based on raw number
+	
+	
+	print('Finished state')
+	emit_signal("end_state")
+
 func check_self_grid(dir):
 	var x = cur_state.self_pos_x
 	var y = cur_state.self_pos_y
