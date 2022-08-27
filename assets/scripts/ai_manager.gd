abaef451b394c20ba989acb860b64a687e4fffce
erik-lance <eriklance@gmail.com>
2022-08-27 22:06:55 +0800

    Create ai_manager.gd

---
+++ assets/scripts/ai_manager.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+signal finished_choosing(cards)
+
+onready var deck = $Deck
+
+var ai = null
+var player_deck = null
+
+var cur_state = {
+	player_pos_x = 0,
+	player_pos_y = 1,
+	self_pos_x = 7,
+	self_pos_y = 2,
+	player_hp = 100,
+	player_mp = 100,
+	self_hp = 100,
+	self_mp = 100
+}
+
+var pred_state = {
+	player_pos_x = 0,
+	player_pos_y = 1,
+	self_pos_x = 7,
+	self_pos_y = 2,
+	player_hp = 100,
+	player_mp = 100,
+	self_hp = 100,
+	self_mp = 100
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func prepare_ai(d, a):
+	player_deck = d
+	var bot = load(a).instance()
+	ai.add_child(bot)
+	ai = bot
+
+func decide_cards():
+	pass
+
+func calculate_heuristic():
+	pass
+
