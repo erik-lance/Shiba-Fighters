def2fb8afe1a1da6ae96516cc83ef0f3a412f896
erik-lance <eriklance@gmail.com>
2022-09-04 18:20:15 +0800

    Prepared state_cluster to prepare starting states
    Problem, forgot that 3 turns doesn't necessarily mean the other player won't move.
    
    There should be 6 states in a cluster. Or we can just simply do two cluster depths.

--- assets/scripts/state_cluster.gd
+++ assets/scripts/state_cluster.gd
@@ -13,7 +13,12 @@ var parent = null
 var depth = 0
 
 var states = []
+var end_states = []
 var cur_calc_state = 0
+
+var turn_two = []
+var turn_three = []
+
 
 var cluster_children = []
 
@@ -29,6 +34,8 @@ var sequence = {
 	move_3 = null
 }
 
+var cur_dets = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -37,6 +44,9 @@ func load_ai(h,a):
 	human_ai = h
 	agent_ai = a
 
+func get_human_ai(): return human_ai
+func get_agent_ai(): return agent_ai
+
 func set_manager(m): root_manager = m
 func set_parent(p): parent = p
 func set_depth(d): depth = d
@@ -44,10 +54,28 @@ func set_move(m): start_move = m
 func set_start_state(s): start_state = s
 func set_end_state(s): end_state = s
 
-func new_state_cluster(start_state,m,p=self,d=depth+1):
+func set_start_dets(d): cur_dets = d
+
+# This is for constructing a new cluster, not to be used
+# inside a cluster, only inside the parent simulator
+func prep_cluster(start_dets, m, p, h, a, d=0):
+	self.set_start_dets(start_dets)
+	self.set_parent(p)
+	self.set_depth(d)
+	self.set_move(m)
+	
+	self.load_ai(h,a)
+	
+	# Begins calculating
+	begin_tree()
+
+# This is only if we want to exceed depth 1
+func new_state_cluster(start_dets,m,p=self,d=depth+1):
 	var new_cluster = load("res://scenes/state_cluster.tscn").instance()
 	root_manager.add_child(new_cluster)
 	cluster_children.append(new_cluster)
+	
+	new_cluster.load_ai(human_ai, agent_ai)
 	
 	new_cluster.set_parent(p)
 	new_cluster.set_depth(d)
@@ -62,5 +90,26 @@ func prep_state(dets, m=-1):
 	states[cur_calc_state].prep_dets(dets, self,m)
 	cur_calc_state += 1
 
+func begin_tree():
+	prep_state(cur_dets, start_move)
+	print('Done tree!')
+
+
+func _on_finish_end_state(state):
+	end_states.append(state)
 
 func get_heuristic_value(): return h_value
+
+# ALPHA-BETA MINIMAX PRUNING
+func alpha_beta_search(s):
+	var v = max_value(s,-INF,INF)
+	pass
+
+func max_value(s, alpha, beta):
+	pass
+
+func min_value(s, alpha, beta):
+	pass
+
+
+
